7d2dc7d1b46582d909fc3cee2575c557ba301878
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-11-16 16:39:33 -0300

    add server file

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
+
+import apiRoutes from './routes/api';
 
 dotenv.config();
 
 const server = express();
 
+server.use(cors());
+
 server.use(express.urlencoded({extended: true}));
 
-// server.use(mainRoutes);
+server.use(apiRoutes);
 
-server.use((_, res) => {
+server.use((req, res) => {
 
-    res.status(404).send('Not found!')
-})
+    res.status(404).json({ error: 'Endpoint não encontrado.' });
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+
+    // Bad Request
+    res.status(400).json({ error: 'Ocorreu algum erro.' });
+    console.log(err);
+
+};
+
+server.use(errorHandler);
 
 server.listen(process.env.PORT, () => {
+
     console.log(`Server running at http://localhost:${process.env.PORT}`);
     
-})
+});

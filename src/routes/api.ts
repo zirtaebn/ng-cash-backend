db6927fad028850f44a8b0dbe8d172b97e463f26
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-11-19 21:26:51 -0300

    add get one transaction endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -8,14 +8,17 @@ import { Auth } from '../middlewares/auth';
 const router = Router();
 
 // user
-router.post('/signup', UserController.signUp); // public route
-router.post('/login', UserController.logIn); // public route
+router.post('/signup', UserController.signup); // public route
+router.post('/login', UserController.login); // public route
 
 // account
 router.get('/:username', Auth.private, AccountController.getAccount); //private route
 
 // transactions
 router.get('/:username/transactions', Auth.private, TransactionController.getTransactions); //private route
+router.get('/:username/transactions/:id', Auth.private, TransactionController.getOneTransaction); //private route
+
+router.get('/:username/transaction', Auth.private, TransactionController.transaction); //private route
 router.post('/:username/transaction', Auth.private, TransactionController.makeTransaction); //private route
 
 
